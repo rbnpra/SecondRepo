1763c0d8fd9931f17902eb5334a12e1b2bf71e92
dev <dev@rabins-iMac.local>
2022-02-10 10:08:10 -0800

    piramiters passed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true,10000, 8, 200);
+
+    }
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
         }
+
     }
 }
